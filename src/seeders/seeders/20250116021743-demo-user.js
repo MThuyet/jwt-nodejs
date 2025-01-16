9801c81dcc48ed7d73b895b2d3a318f40db2862e
psychisch <mthuyet294@gmail.com>
2025-01-16 10:12:29 +0700

    generate model/migrations/seeder

---
+++ src/seeders/seeders/20250116021743-demo-user.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const user = require('../../model/models/user');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    // hardcode dữ liệu mẫu vào db
+    await queryInterface.bulkInsert(
+      // hàm bulkInsert để insert nhiều row vào db cùng lúc
+      'Users',
+      [
+        {
+          email: 'John Doe',
+          password: '12345',
+          username: 'fake1',
+        },
+        {
+          email: 'John Doe2',
+          password: '12345',
+          username: 'fake2',
+        },
+        {
+          email: 'John Doe3',
+          password: '12345',
+          username: 'fake3',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ import express from 'express';
 import configViewEngine from './config/ViewEngine';
 import initWebRoutes from './routes/web';
 import bodyParser from 'body-parser';
+import Connection from './config/ConnectDB';
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// test connect DB
+Connection();
 
 // config body parser
 app.use(bodyParser.urlencoded()); // parse application/x-www-form-urlencoded

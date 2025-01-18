7ed77922b7edde72f6d661f731f8d7e80ab405ea
psychisch <mthuyet294@gmail.com>
2025-01-18 09:03:22 +0700

    fix CROSS

--- src/server.js
+++ src/server.js
@@ -4,11 +4,30 @@ import initWebRoutes from './routes/web';
 import bodyParser from 'body-parser';
 import Connection from './config/ConnectDB';
 
+const app = express();
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+// CORS
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // test connect DB
 Connection();

f2cb383c0d25beb90132f671d0de3c4593d360c8
psychisch <mthuyet294@gmail.com>
2025-01-31 20:03:38 +0700

    demo use JWT

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import initApiRoutes from './routes/api';
 import bodyParser from 'body-parser';
 import Connection from './config/ConnectDB';
 import configCORS from './config/Cors';
+import { createJWT, verifyToken } from './middleware/JWTAction';
 
 const app = express();
 
@@ -17,6 +18,13 @@ configCORS(app);
 
 // test connect DB
 Connection();
+
+// test JWT
+createJWT();
+let decodedData = verifyToken(
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVEhVWUVUIiwiYWdlIjoxOSwiaWF0IjoxNzM4MzI4MTA5fQ.3qVLc8dqRLz5aeq7yi1gtWCuWSfIJ_RjSweORNk0TKw',
+);
+console.log('check decoded data: ', decodedData);
 
 // config body parser
 app.use(bodyParser.urlencoded()); // parse application/x-www-form-urlencoded
